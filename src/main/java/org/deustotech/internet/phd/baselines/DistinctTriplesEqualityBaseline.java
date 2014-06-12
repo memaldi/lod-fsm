7d2c1843b2090f9d72d9438c96c1c880f058e274
Mikel Emaldi <m.emaldi@gmail.com>
2014-06-12 10:29:50 +0200

    Implemented predicate checking

--- src/main/java/org/deustotech/internet/phd/baselines/DistinctTriplesEqualityBaseline.java
+++ src/main/java/org/deustotech/internet/phd/baselines/DistinctTriplesEqualityBaseline.java
@@ -5,8 +5,6 @@ import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.RDFNode;
-import com.hp.hpl.jena.vocabulary.RDF;
-import javafx.util.Pair;
 import org.apache.http.client.utils.URIBuilder;
 import virtuoso.jena.driver.VirtGraph;
 import virtuoso.jena.driver.VirtuosoQueryExecution;
@@ -18,12 +16,12 @@ import java.net.URISyntaxException;
 import java.util.*;
 
 /**
- * Created by mikel on 11/06/14.
+ * Created by mikel (m.emaldi at deusto dot es) (m.emaldi at deusto dot es) on 11/06/14.
  */
 public class DistinctTriplesEqualityBaseline {
     public void launch(String csvLocation) {
         Properties prop = new Properties();
-        InputStream input = null;
+        InputStream input;
         try {
             input = getClass().getResourceAsStream("/config.properties");
             prop.load(input);
@@ -38,7 +36,7 @@ public class DistinctTriplesEqualityBaseline {
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
-        BufferedReader br = null;
+        BufferedReader br;
         Set<String> datasetList = new HashSet<String>();
         try {
             br = new BufferedReader(new FileReader(csvLocation));
@@ -50,38 +48,53 @@ public class DistinctTriplesEqualityBaseline {
             br.close();
             Set<String> doneList = new HashSet<String>();
             Map<String, Map<String, Integer>> commonMap = new HashMap<String, Map<String, Integer>>();
+            Map<String, Set<String>> exclusionMap = new HashMap<String, Set<String>>();
             for (String sourceDataset : datasetList) {
                 Map<String, Integer> map = new HashMap<String, Integer>();
+                assert connectionURL != null;
                 VirtGraph graph = new VirtGraph("http://" + sourceDataset, connectionURL.toString(), prop.getProperty("virtuoso_user"), prop.getProperty("virtuoso_password"));
                 Query query = QueryFactory.create("SELECT DISTINCT ?p ?o WHERE { ?s ?p ?o }");
                 VirtuosoQueryExecution vqe = VirtuosoQueryExecutionFactory.create(query, graph);
                 ResultSet results = vqe.execSelect();
-                while(results.hasNext()) {
+                while (results.hasNext()) {
                     QuerySolution result = results.next();
                     RDFNode predicate = result.get("p");
                     RDFNode object = result.get("o");
+                    System.out.println(String.format("%s - %s", predicate, object));
+                    if (!exclusionMap.containsKey(predicate.toString())) {
+                        Set<String> datasetSet = new HashSet<String>();
+                        for (String dataset : datasetList) {
+                            if (!dataset.equals(sourceDataset)) {
+                                VirtGraph targetGraph = new VirtGraph("http://" + dataset, connectionURL.toString(), prop.getProperty("virtuoso_user"), prop.getProperty("virtuoso_password"));
+                                Query predicateQuery = QueryFactory.create(String.format("ASK {?s <%s> ?o}", predicate));
+                                VirtuosoQueryExecution vqePredicate = VirtuosoQueryExecutionFactory.create(predicateQuery, targetGraph);
+                                boolean ask = vqePredicate.execAsk();
+                                if (!ask) {
+                                    datasetSet.add(dataset);
+                                }
+                            }
+                        }
+
+                        exclusionMap.put(predicate.toString(), datasetSet);
+                    }
+
                     for (String targetDataset : datasetList) {
-                        if ((!doneList.contains(targetDataset)) && (!targetDataset.equals(sourceDataset))) {
+                        if ((!doneList.contains(targetDataset)) && (!targetDataset.equals(sourceDataset)) && (!exclusionMap.get(predicate.toString()).contains(targetDataset))) {
                             VirtGraph targetGraph = new VirtGraph("http://" + targetDataset, connectionURL.toString(), prop.getProperty("virtuoso_user"), prop.getProperty("virtuoso_password"));
-                            Query targetQuery = null;
+                            Query targetQuery;
                             if (object.isLiteral()) {
-                                targetQuery = QueryFactory.create(String.format("ASK { ?s <%s> \"%s\"}", predicate, object));
+                                targetQuery = QueryFactory.create(String.format("ASK { ?s <%s> \"%s\"}", predicate, object.toString().replace("\"", "\\\"")));
                             } else {
                                 targetQuery = QueryFactory.create(String.format("ASK { ?s <%s> <%s>}", predicate, object));
                             }
                             VirtuosoQueryExecution targetVqe = VirtuosoQueryExecutionFactory.create(targetQuery, targetGraph);
-                            ResultSet targetResults = targetVqe.execSelect();
-                            while(targetResults.hasNext()){
-                                QuerySolution targetResult = targetResults.next();
-                                RDFNode ask = targetResult.get("__ask_retval");
-                                if (ask.toString().equals("1^^http://www.w3.org/2001/XMLSchema#integer")) {
-                                    int count = 0;
-                                    if (map.containsKey(targetDataset)) {
-                                        count = map.get(targetDataset);
-                                    }
-                                    map.put(targetDataset, ++count);
+                            boolean ask = targetVqe.execAsk();
+                            if (ask) {
+                                int count = 0;
+                                if (map.containsKey(targetDataset)) {
+                                    count = map.get(targetDataset);
                                 }
-
+                                map.put(targetDataset, ++count);
                             }
                             targetGraph.close();
                         }
