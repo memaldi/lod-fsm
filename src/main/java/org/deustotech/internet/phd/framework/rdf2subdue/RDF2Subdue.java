4246117654eb90ae459735c552b139bfc14a4a75
Mikel Emaldi <m.emaldi@gmail.com>
2014-06-30 18:16:44 +0200

    Reimplementing RDF2Subdue in a more efficient way. A lot of problems with blank nodes

--- src/main/java/org/deustotech/internet/phd/framework/rdf2subdue/RDF2Subdue.java
+++ src/main/java/org/deustotech/internet/phd/framework/rdf2subdue/RDF2Subdue.java
@@ -4,6 +4,7 @@ import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.vocabulary.RDF;
 import org.apache.hadoop.conf.Configuration;
@@ -25,10 +26,7 @@ import virtuoso.jena.driver.VirtuosoQueryExecutionFactory;
 import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
-import java.util.UUID;
+import java.util.*;
 import java.util.logging.Logger;
 
 /**
@@ -194,144 +192,102 @@ public class RDF2Subdue {
         }
 
         VirtGraph graph = new VirtGraph("http://" + dataset, connectionURL.toString(), prop.getProperty("virtuoso_user"), prop.getProperty("virtuoso_password"));
-        Query query = QueryFactory.create("SELECT DISTINCT ?s ?p ?o ?class WHERE { ?s ?p ?o OPTIONAL { ?o a ?class } } ORDER BY ?s OFFSET " + offset);
+        Query query = QueryFactory.create("SELECT DISTINCT ?s ?class WHERE {?s a ?class} ORDER BY ?s OFFSET" + offset);
         VirtuosoQueryExecution vqe = VirtuosoQueryExecutionFactory.create(query, graph);
         ResultSet results = vqe.execSelect();
-        logger.info("Generating vertices and edges ids...");
+        logger.info("Generating vertices...");
+
+        Map<String, List<Long>> vertexMap = new HashMap<>();
 
         long id = 1;
 
-        while (results.hasNext()) {
+        while(results.hasNext()) {
             QuerySolution result = results.next();
-            RDFNode subject = result.get("s");
-            RDFNode predicate = result.get("p");
-            RDFNode object = result.get("o");
+            String subject = result.getResource("s").getURI();
+            String clazz = result.getResource("class").getURI();
 
-            if (predicate.equals(RDF.type)) {
-                Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
-                put.add(Bytes.toBytes("cf"), Bytes.toBytes("uri"), Bytes.toBytes(subject.toString()));
-                put.add(Bytes.toBytes("cf"), Bytes.toBytes("label"), Bytes.toBytes(object.toString()));
-                put.add(Bytes.toBytes("cf"), Bytes.toBytes("type"), Bytes.toBytes("vertex"));
-                put.add(Bytes.toBytes("cf"), Bytes.toBytes("id"), Bytes.toBytes(id));
-                try {
-                    table.put(put);
-                } catch (InterruptedIOException e) {
-                    e.printStackTrace();
-                } catch (RetriesExhaustedWithDetailsException e) {
-                    e.printStackTrace();
-                }
-                id++;
-            } else if (object.isResource()) {
-                List<Filter> filterList = new ArrayList<Filter>();
-                SingleColumnValueFilter filter = new SingleColumnValueFilter(Bytes.toBytes("cf"), Bytes.toBytes("uri"), CompareFilter.CompareOp.EQUAL, Bytes.toBytes(object.toString()));
-                filter.setFilterIfMissing(true);
-                filterList.add(filter);
-                FilterList fl = new FilterList(FilterList.Operator.MUST_PASS_ALL, filterList);
-                Scan scan = new Scan();
-                scan.setFilter(fl);
-                try {
-                    ResultScanner scanner = table.getScanner(scan);
-                    Result scannerResult;
-                    List<Long> idList = new ArrayList<Long>();
-
-                    while((scannerResult = scanner.next()) != null) {
-                        byte[] objectId = scannerResult.getValue(Bytes.toBytes("cf"), Bytes.toBytes("id"));
-                        idList.add(Bytes.toLong(objectId));
-                    }
-                    byte[] sourceId = null;
-                    List<Filter> filterList2 = new ArrayList<Filter>();
-                    SingleColumnValueFilter filter2 = new SingleColumnValueFilter(Bytes.toBytes("cf"), Bytes.toBytes("uri"), CompareFilter.CompareOp.EQUAL, Bytes.toBytes(subject.toString()));
-                    filter2.setFilterIfMissing(true);
-                    filterList2.add(filter2);
-                    Filter fl2 = new FilterList(FilterList.Operator.MUST_PASS_ALL, filterList2);
-                    Scan scan2 = new Scan();
-                    scan2.setFilter(fl2);
-                    ResultScanner scanner2 = table.getScanner(scan2);
-                    Result scannerResult2;
-
-                    while((scannerResult2 = scanner2.next()) != null) {
-                        sourceId = scannerResult2.getValue(Bytes.toBytes("cf"), Bytes.toBytes("id"));
-                    }
-                    scanner2.close();
-                    if (idList.size() == 0) {
-                        if (result.contains("class")) {
-                            RDFNode rdfType = result.get("class");
-                            Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("uri"), Bytes.toBytes(object.toString()));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("label"), Bytes.toBytes(rdfType.toString()));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("type"), Bytes.toBytes("vertex"));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("id"), Bytes.toBytes(id));
-                        } else {
-                            Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("label"), Bytes.toBytes(object.toString()));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("type"), Bytes.toBytes("vertex"));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("id"), Bytes.toBytes(id));
-                            table.put(put);
-                        }
-
-                        Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
-                        put.add(Bytes.toBytes("cf"), Bytes.toBytes("source"), sourceId);
-                        put.add(Bytes.toBytes("cf"), Bytes.toBytes("target"), Bytes.toBytes(id));
-                        put.add(Bytes.toBytes("cf"), Bytes.toBytes("label"), Bytes.toBytes(predicate.toString()));
-                        put.add(Bytes.toBytes("cf"), Bytes.toBytes("type"), Bytes.toBytes("edge"));
-                        table.put(put);
-                        id++;
-                    } else {
-                        for (Long targetId : idList) {
-                            Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("source"), sourceId);
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("target"), Bytes.toBytes(targetId));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("label"), Bytes.toBytes(predicate.toString()));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("type"), Bytes.toBytes("edge"));
-                            table.put(put);
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            } else {
-                Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
-                put.add(Bytes.toBytes("cf"), Bytes.toBytes("label"), Bytes.toBytes(object.toString()));
-                put.add(Bytes.toBytes("cf"), Bytes.toBytes("type"), Bytes.toBytes("vertex"));
-                put.add(Bytes.toBytes("cf"), Bytes.toBytes("id"), Bytes.toBytes(id));
-                try {
-                    table.put(put);
-                } catch (InterruptedIOException e) {
-                    e.printStackTrace();
-                } catch (RetriesExhaustedWithDetailsException e) {
-                    e.printStackTrace();
-                }
-
-                List<Filter> filterList = new ArrayList<Filter>();
-                SingleColumnValueFilter filter = new SingleColumnValueFilter(Bytes.toBytes("cf"), Bytes.toBytes("uri"), CompareFilter.CompareOp.EQUAL, Bytes.toBytes(subject.toString()));
-                filter.setFilterIfMissing(true);
-                filterList.add(filter);
-                FilterList fl = new FilterList(FilterList.Operator.MUST_PASS_ALL, filterList);
-                Scan scan = new Scan();
-                scan.setFilter(fl);
-                try {
-                    ResultScanner scanner = table.getScanner(scan);
-                    Result scannerResult;
-                    while((scannerResult = scanner.next()) != null) {
-                        byte[] sourceId = scannerResult.getValue(Bytes.toBytes("cf"), Bytes.toBytes("id"));
-                        put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
-                        put.add(Bytes.toBytes("cf"), Bytes.toBytes("source"), sourceId);
-                        put.add(Bytes.toBytes("cf"), Bytes.toBytes("target"), Bytes.toBytes(id));
-                        put.add(Bytes.toBytes("cf"), Bytes.toBytes("label"), Bytes.toBytes(predicate.toString()));
-                        put.add(Bytes.toBytes("cf"), Bytes.toBytes("type"), Bytes.toBytes("edge"));
-                        table.put(put);
-                    }
-                    scanner.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                id++;
+            if (!vertexMap.containsKey(subject)) {
+                vertexMap.put(subject, new ArrayList<Long>());
             }
+
+            vertexMap.get(subject).add(id);
+
+            Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
+            put.add(Bytes.toBytes("cf"), Bytes.toBytes("id"), Bytes.toBytes(id));
+            put.add(Bytes.toBytes("cf"), Bytes.toBytes("label"), Bytes.toBytes(clazz));
+
+            try {
+                table.put(put);
+            } catch (InterruptedIOException e) {
+                e.printStackTrace();
+            } catch (RetriesExhaustedWithDetailsException e) {
+                e.printStackTrace();
+            }
+            id++;
+
             offset++;
             jedis.set(String.format("rdf2subdue:%s:offset", dataset), String.valueOf(offset));
         }
+        vqe.close();
+
+        query = QueryFactory.create("SELECT DISTINCT ?o WHERE { ?s ?p ?o . FILTER EXISTS { ?s a ?class } . FILTER NOT EXISTS { ?o ?p2 ?o2 } } ORDER BY ?o");
+        vqe = VirtuosoQueryExecutionFactory.create(query, graph);
+        results = vqe.execSelect();
+
+        while(results.hasNext()) {
+            QuerySolution result = results.next();
+            String object = result.get("o").toString();
+            if (!vertexMap.containsKey(object)) {
+                List<Long> idList = new ArrayList<>();
+                idList.add(id);
+                vertexMap.put(object, idList);
+                Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
+                put.add(Bytes.toBytes("cf"), Bytes.toBytes("id"), Bytes.toBytes(id));
+                put.add(Bytes.toBytes("cf"), Bytes.toBytes("label"), Bytes.toBytes(object));
+
+                try {
+                    table.put(put);
+                } catch (InterruptedIOException e) {
+                    e.printStackTrace();
+                } catch (RetriesExhaustedWithDetailsException e) {
+                    e.printStackTrace();
+                }
+                id++;
+            }
+        }
+        vqe.close();
+
+        query = QueryFactory.create("SELECT DISTINCT ?s ?p ?o WHERE { ?s ?p ?o . FILTER EXISTS { ?s a ?class } } ORDER BY ?s");
+        vqe = VirtuosoQueryExecutionFactory.create(query, graph);
+        results = vqe.execSelect();
+
+        while (results.hasNext()) {
+            QuerySolution result = results.next();
+            List<Long> sourceIdList = vertexMap.get(result.get("s").toString());
+            String predicate = result.get("p").toString();
+            if (!predicate.equals(RDF.type.getURI())) {
+                for (long sourceId : sourceIdList) {
+                    List<Long> targetIdList = vertexMap.get(result.get("o").toString());
+                    if (targetIdList != null) {
+                        for (long targetId : targetIdList) {
+                            Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
+                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("source"), Bytes.toBytes(sourceId));
+                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("target"), Bytes.toBytes(targetId));
+                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("label"), Bytes.toBytes(predicate));
+
+                            try {
+                                table.put(put);
+                            } catch (InterruptedIOException e) {
+                                e.printStackTrace();
+                            } catch (RetriesExhaustedWithDetailsException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         try {
             table.close();
             connection.close();
