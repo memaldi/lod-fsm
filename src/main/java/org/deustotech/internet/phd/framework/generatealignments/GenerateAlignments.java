2544d46144b27d620e8274e40dafd7b0f1e2f801
Mikel Emaldi <m.emaldi@gmail.com>
2014-08-27 13:16:22 +0200

    Replacing htable by hypertable in GenerateAlignments.

--- src/main/java/org/deustotech/internet/phd/framework/generatealignments/GenerateAlignments.java
+++ src/main/java/org/deustotech/internet/phd/framework/generatealignments/GenerateAlignments.java
@@ -3,23 +3,21 @@ package org.deustotech.internet.phd.framework.generatealignments;
 import net.ericaro.neoitertools.Generator;
 import net.ericaro.neoitertools.Itertools;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.HColumnDescriptor;
-import org.apache.hadoop.hbase.HTableDescriptor;
-import org.apache.hadoop.hbase.client.*;
+
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
 import java.util.*;
 import java.util.logging.Logger;
 import fr.inrialpes.exmo.ontosim.string.StringDistances;
 import fr.inrialpes.exmo.ontosim.string.JWNLDistances;
-import org.apache.hadoop.hbase.filter.CompareFilter;
-import org.apache.hadoop.hbase.filter.Filter;
-import org.apache.hadoop.hbase.filter.FilterList;
-import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
-import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.thrift.TException;
+import org.hypertable.thrift.ThriftClient;
+import org.hypertable.thriftgen.*;
+
 
 /**
  * Created by mikel (m.emaldi at deusto dot es) on 18/06/14.
@@ -39,114 +37,297 @@ public class GenerateAlignments {
 
     public static void run(String wordnetDir, String wordnetVersion) {
         Logger logger = Logger.getLogger(GenerateAlignments.class.getName());
-        Configuration conf = HBaseConfiguration.create();
-        HTable alignmentTable = null;
-        HTable subgraphTable = null;
-        logger.info("Creating table \"alignments\"...");
-        HBaseAdmin hbase = null;
+        ThriftClient client = null;
         try {
-            hbase = new HBaseAdmin(conf);
-            HTableDescriptor desc = new HTableDescriptor("alignments");
-            HColumnDescriptor meta = new HColumnDescriptor("cf".getBytes());
-            desc.addFamily(meta);
-            hbase.createTable(desc);
-        } catch (IOException e) {
-            logger.info("Table \"alignments\" already exists!");
+            client = ThriftClient.create("localhost", 15867);
+        } catch (TException e) {
+            System.exit(1);
         }
+
+        long ns = 0;
         try {
-            subgraphTable = new HTable(conf, "subgraphs");
-            alignmentTable = new HTable(conf, "alignments");
-        } catch (IOException e) {
+            if (!client.namespace_exists("framework")) {
+                client.namespace_create("framework");
+            }
+            ns = client.namespace_open("framework");
+        } catch (TException e) {
+            e.printStackTrace();
+        }
+
+        Schema schema = new Schema();
+
+        Map columnFamilies = new HashMap();
+
+        ColumnFamilySpec cf = new ColumnFamilySpec();
+        cf.setName("source");
+        columnFamilies.put("source", cf);
+
+        cf = new ColumnFamilySpec();
+        cf.setName("target");
+        columnFamilies.put("target", cf);
+
+        cf = new ColumnFamilySpec();
+        cf.setName("distance");
+        columnFamilies.put("distance", cf);
+
+        cf = new ColumnFamilySpec();
+        cf.setName("value");
+        columnFamilies.put("value", cf);
+
+        schema.setColumn_families(columnFamilies);
+
+        try {
+            client.table_create(ns, "alignments", schema);
+        } catch (TException e) {
             e.printStackTrace();
         }
 
         logger.info("Aligning vertices...");
-        generateSimilarities(alignmentTable, subgraphTable, "vertex", wordnetDir, wordnetVersion);
+        generateSimilarities(client, ns, "vertex", wordnetDir, wordnetVersion);
         logger.info("Aligning edges...");
-        generateSimilarities(alignmentTable, subgraphTable, "edge", wordnetDir, wordnetVersion);
+        generateSimilarities(client, ns, "edge", wordnetDir, wordnetVersion);
         logger.info("Done!");
-        try {
-            alignmentTable.close();
-            subgraphTable.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        client.close();
 
     }
 
-    private static void generateSimilarities(HTable alignmentTable, HTable subgraphTable, String type, String wordnetDir, String wordnetVersion) {
-        List<Filter> filterList = new ArrayList<>();
-        SingleColumnValueFilter filter = new SingleColumnValueFilter(Bytes.toBytes("cf"), Bytes.toBytes("type"), CompareFilter.CompareOp.EQUAL, Bytes.toBytes(type));
-        filter.setFilterIfMissing(true);
-        filterList.add(filter);
-        FilterList fl = new FilterList(FilterList.Operator.MUST_PASS_ALL, filterList);
-        Scan scan = new Scan();
-        scan.setFilter(fl);
+    private static void generateSimilarities(ThriftClient client, long ns, String type, String wordnetDir, String wordnetVersion) {
+
+        String query = String.format("SELECT * from subgraphs where type = '%s'", type);
+
         Set<String> vertexSet = new HashSet<>();
         try {
-            ResultScanner scanner = subgraphTable.getScanner(scan);
-            Result result;
-            while ((result = scanner.next()) != null) {
-                String label = Bytes.toString(result.getValue(Bytes.toBytes("cf"), Bytes.toBytes("label")));
-                vertexSet.add(label.replace("<", "").replace(">", ""));
-            }
-            scanner.close();
-
-            Generator<List<String>> labelPermutations = Itertools.permutations(Itertools.iter(vertexSet.iterator()), 2);
-            List<String> pair;
-            Class<StringDistances> stringDistancesClass = StringDistances.class;
-            JWNLDistances jwnlDistances = new JWNLDistances();
-            jwnlDistances.Initialize(wordnetDir, wordnetVersion);
-            Class[] cArg = new Class[2];
-            cArg[0] = String.class;
-            cArg[1] = String.class;
-            try {
-                while ((pair = labelPermutations.next()) != null) {
-                    if (!getNamespace(pair.get(0)).equals(getNamespace(pair.get(1)))) {
-                        double accum = 0.0;
-
-                        for (String strDistance : STRING_DISTANCES) {
-                            Method method = stringDistancesClass.getMethod(strDistance, cArg);
-                            double distance = (double) method.invoke(stringDistancesClass, getLocalName(pair.get(0)), getLocalName(pair.get(1)));
-                            Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("source"), Bytes.toBytes(pair.get(0)));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("target"), Bytes.toBytes(pair.get(1)));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("distance"), Bytes.toBytes(strDistance));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("value"), Bytes.toBytes(distance));
-                            alignmentTable.put(put);
-                            accum += distance;
-                        }
-                        for (String jwnlDistance : JWNL_DISTANCES) {
-                            Method method = jwnlDistances.getClass().getMethod(jwnlDistance, cArg);
-                            double distance = (double) method.invoke(jwnlDistances, getLocalName(pair.get(0)), getLocalName(pair.get(1)));
-                            Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("source"), Bytes.toBytes(pair.get(0)));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("target"), Bytes.toBytes(pair.get(1)));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("distance"), Bytes.toBytes(jwnlDistance));
-                            put.add(Bytes.toBytes("cf"), Bytes.toBytes("value"), Bytes.toBytes(distance));
-                            alignmentTable.put(put);
-                            accum += distance;
-                        }
-                        Put put = new Put(Bytes.toBytes(UUID.randomUUID().toString()));
-                        double geometricMean = accum / (STRING_DISTANCES.length + JWNL_DISTANCES.length);
-                        put.add(Bytes.toBytes("cf"), Bytes.toBytes("source"), Bytes.toBytes(pair.get(0)));
-                        put.add(Bytes.toBytes("cf"), Bytes.toBytes("target"), Bytes.toBytes(pair.get(1)));
-                        put.add(Bytes.toBytes("cf"), Bytes.toBytes("distance"), Bytes.toBytes("geometricMean"));
-                        put.add(Bytes.toBytes("cf"), Bytes.toBytes("value"), Bytes.toBytes(geometricMean));
-                        alignmentTable.put(put);
-                    }
+            HqlResult hqlResult = client.hql_query(ns, query);
+            if (hqlResult.getCells().size() > 0) {
+                for (Cell cell : hqlResult.getCells()) {
+                    ByteBuffer labelBuffer = client.get_cell(ns, "subgraphs", cell.getKey().getRow(), "label");
+                    String label = new String(labelBuffer.array(), labelBuffer.position(), labelBuffer.remaining());
+                    vertexSet.add(label.replace("<", "").replace(">", ""));
                 }
-            } catch (NoSuchElementException e) {
-                // Well, permutations.next() do not return null when the last element is reached.
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
             }
+        } catch (ClientException e) {
+            e.printStackTrace();
+        } catch (TException e) {
+            e.printStackTrace();
+        }
 
-        } catch (IOException e) {
+        Generator<List<String>> labelPermutations = Itertools.permutations(Itertools.iter(vertexSet.iterator()), 2);
+        List<String> pair;
+        Class<StringDistances> stringDistancesClass = StringDistances.class;
+        JWNLDistances jwnlDistances = new JWNLDistances();
+        jwnlDistances.Initialize(wordnetDir, wordnetVersion);
+        Class[] cArg = new Class[2];
+        cArg[0] = String.class;
+        cArg[1] = String.class;
+        try {
+            List<Cell> cells = new ArrayList<>();
+            while ((pair = labelPermutations.next()) != null) {
+                if (!getNamespace(pair.get(0)).equals(getNamespace(pair.get(1)))) {
+                    double accum = 0.0;
+
+                    for (String strDistance : STRING_DISTANCES) {
+                        Method method = stringDistancesClass.getMethod(strDistance, cArg);
+                        double distance = (double) method.invoke(stringDistancesClass, getLocalName(pair.get(0)), getLocalName(pair.get(1)));
+
+                        String keyID = UUID.randomUUID().toString();
+                        Key key = new Key();
+                        key.setRow(keyID);
+                        key.setColumn_family("source");
+                        Cell cell = new Cell();
+                        cell.setKey(key);
+
+                        try {
+                            cell.setValue(pair.get(0).getBytes("UTF-8"));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+
+                        cells.add(cell);
+
+                        key = new Key();
+                        key.setRow(keyID);
+                        key.setColumn_family("target");
+                        cell = new Cell();
+                        cell.setKey(key);
+
+                        try {
+                            cell.setValue(pair.get(1).getBytes("UTF-8"));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+
+                        cells.add(cell);
+
+                        key = new Key();
+                        key.setRow(keyID);
+                        key.setColumn_family("distance");
+                        cell = new Cell();
+                        cell.setKey(key);
+
+                        try {
+                            cell.setValue(strDistance.getBytes("UTF-8"));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+
+                        cells.add(cell);
+
+                        key = new Key();
+                        key.setRow(keyID);
+                        key.setColumn_family("value");
+                        cell = new Cell();
+                        cell.setKey(key);
+
+                        try {
+                            cell.setValue(String.valueOf(distance).getBytes("UTF-8"));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+
+                        cells.add(cell);
+
+                        accum += distance;
+                    }
+                    for (String jwnlDistance : JWNL_DISTANCES) {
+                        Method method = jwnlDistances.getClass().getMethod(jwnlDistance, cArg);
+                        double distance = (double) method.invoke(jwnlDistances, getLocalName(pair.get(0)), getLocalName(pair.get(1)));
+
+                        String keyID = UUID.randomUUID().toString();
+                        Key key = new Key();
+                        key.setRow(keyID);
+                        key.setColumn_family("source");
+                        Cell cell = new Cell();
+                        cell.setKey(key);
+
+                        try {
+                            cell.setValue(pair.get(0).getBytes("UTF-8"));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+
+                        cells.add(cell);
+
+                        key = new Key();
+                        key.setRow(keyID);
+                        key.setColumn_family("target");
+                        cell = new Cell();
+                        cell.setKey(key);
+
+                        try {
+                            cell.setValue(pair.get(1).getBytes("UTF-8"));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+
+                        cells.add(cell);
+
+                        key = new Key();
+                        key.setRow(keyID);
+                        key.setColumn_family("distance");
+                        cell = new Cell();
+                        cell.setKey(key);
+
+                        try {
+                            cell.setValue(jwnlDistance.getBytes("UTF-8"));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+
+                        cells.add(cell);
+
+                        cells.add(cell);
+
+                        key = new Key();
+                        key.setRow(keyID);
+                        key.setColumn_family("value");
+                        cell = new Cell();
+                        cell.setKey(key);
+
+                        try {
+                            cell.setValue(String.valueOf(distance).getBytes("UTF-8"));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+
+                        cells.add(cell);
+
+                        accum += distance;
+                    }
+                    double geometricMean = accum / (STRING_DISTANCES.length + JWNL_DISTANCES.length);
+
+                    String keyID = UUID.randomUUID().toString();
+                    Key key = new Key();
+                    key.setRow(keyID);
+                    key.setColumn_family("source");
+                    Cell cell = new Cell();
+                    cell.setKey(key);
+
+                    try {
+                        cell.setValue(pair.get(0).getBytes("UTF-8"));
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+
+                    cells.add(cell);
+
+                    key = new Key();
+                    key.setRow(keyID);
+                    key.setColumn_family("target");
+                    cell = new Cell();
+                    cell.setKey(key);
+
+                    try {
+                        cell.setValue(pair.get(1).getBytes("UTF-8"));
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+
+                    cells.add(cell);
+
+                    key = new Key();
+                    key.setRow(keyID);
+                    key.setColumn_family("distance");
+                    cell = new Cell();
+                    cell.setKey(key);
+
+                    try {
+                        cell.setValue("geometricMean".getBytes("UTF-8"));
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+
+                    cells.add(cell);
+
+                    cells.add(cell);
+
+                    key = new Key();
+                    key.setRow(keyID);
+                    key.setColumn_family("value");
+                    cell = new Cell();
+                    cell.setKey(key);
+
+                    try {
+                        cell.setValue(String.valueOf(geometricMean).getBytes("UTF-8"));
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+
+                    cells.add(cell);
+                }
+            }
+            client.set_cells(ns, "alignments", cells);
+        } catch (NoSuchElementException e) {
+            // Well, permutations.next() do not return null when the last element is reached.
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClientException e) {
+            e.printStackTrace();
+        } catch (TException e) {
             e.printStackTrace();
         }
     }
