266b0d576a56d70aa11c1b9985851df2b70c51ce
Mikel Emaldi <m.emaldi@gmail.com>
2014-12-16 12:28:39 +0100

    Replaced csv file in DistinctTriplesEqualityBaseline

--- src/main/java/org/deustotech/internet/phd/baselines/DistinctTriplesEqualityBaseline.java
+++ src/main/java/org/deustotech/internet/phd/baselines/DistinctTriplesEqualityBaseline.java
@@ -6,6 +6,10 @@ import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.thrift.TException;
+import org.hypertable.thrift.ThriftClient;
+import org.hypertable.thriftgen.Cell;
+import org.hypertable.thriftgen.HqlResult;
 import virtuoso.jena.driver.VirtGraph;
 import virtuoso.jena.driver.VirtuosoQueryExecution;
 import virtuoso.jena.driver.VirtuosoQueryExecutionFactory;
@@ -13,6 +17,7 @@ import virtuoso.jena.driver.VirtuosoQueryExecutionFactory;
 import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
 import java.util.*;
 import java.util.logging.Logger;
 
@@ -20,7 +25,7 @@ import java.util.logging.Logger;
  * Created by mikel (m.emaldi at deusto dot es) (m.emaldi at deusto dot es) on 11/06/14.
  */
 public class DistinctTriplesEqualityBaseline {
-    public void launch(String csvLocation) {
+    public void run() {
         Logger logger = Logger.getLogger(this.getClass().getName());
         logger.info("Initializing...");
         Properties prop = new Properties();
@@ -39,80 +44,104 @@ public class DistinctTriplesEqualityBaseline {
         } catch (URISyntaxException e) {
             e.printStackTrace();
         }
-        BufferedReader br;
         Set<String> datasetList = new HashSet<String>();
+
+        ThriftClient client = null;
         try {
-            br = new BufferedReader(new FileReader(csvLocation));
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] sline = line.split(",");
-                datasetList.add(sline[4]);
-            }
-            br.close();
-            Set<String> doneList = new HashSet<String>();
-            Map<String, Map<String, Integer>> commonMap = new HashMap<String, Map<String, Integer>>();
-            Map<String, Set<String>> exclusionMap = new HashMap<String, Set<String>>();
-            for (String sourceDataset : datasetList) {
-                logger.info(String.format("Analyzing %s dataset...", sourceDataset));
-                Map<String, Integer> map = new HashMap<String, Integer>();
-                assert connectionURL != null;
-                VirtGraph graph = new VirtGraph("http://" + sourceDataset, connectionURL.toString(), prop.getProperty("virtuoso_user"), prop.getProperty("virtuoso_password"));
-                Query query = QueryFactory.create("SELECT DISTINCT ?p ?o WHERE { ?s ?p ?o }");
-                VirtuosoQueryExecution vqe = VirtuosoQueryExecutionFactory.create(query, graph);
-                ResultSet results = vqe.execSelect();
-                while (results.hasNext()) {
-                    QuerySolution result = results.next();
-                    RDFNode predicate = result.get("p");
-                    RDFNode object = result.get("o");
-                    //System.out.println(String.format("%s - %s", predicate, object));
-                    if (!exclusionMap.containsKey(predicate.toString())) {
-                        Set<String> datasetSet = new HashSet<String>();
-                        for (String dataset : datasetList) {
-                            if (!dataset.equals(sourceDataset)) {
-                                VirtGraph targetGraph = new VirtGraph("http://" + dataset, connectionURL.toString(), prop.getProperty("virtuoso_user"), prop.getProperty("virtuoso_password"));
-                                Query predicateQuery = QueryFactory.create(String.format("ASK {?s <%s> ?o}", predicate));
-                                VirtuosoQueryExecution vqePredicate = VirtuosoQueryExecutionFactory.create(predicateQuery, targetGraph);
-                                boolean ask = vqePredicate.execAsk();
-                                if (!ask) {
-                                    datasetSet.add(dataset);
-                                }
-                            }
-                        }
-
-                        exclusionMap.put(predicate.toString(), datasetSet);
-                    }
-
-                    for (String targetDataset : datasetList) {
-                        if ((!doneList.contains(targetDataset)) && (!targetDataset.equals(sourceDataset)) && (!exclusionMap.get(predicate.toString()).contains(targetDataset))) {
-                            VirtGraph targetGraph = new VirtGraph("http://" + targetDataset, connectionURL.toString(), prop.getProperty("virtuoso_user"), prop.getProperty("virtuoso_password"));
-                            Query targetQuery;
-                            if (object.isLiteral()) {
-                                targetQuery = QueryFactory.create(String.format("ASK { ?s <%s> \"%s\"}", predicate, object.toString().replace("\"", "").replace("\\", "")));
-                            } else {
-                                targetQuery = QueryFactory.create(String.format("ASK { ?s <%s> <%s>}", predicate, object));
-                            }
-                            VirtuosoQueryExecution targetVqe = VirtuosoQueryExecutionFactory.create(targetQuery, targetGraph);
-                            boolean ask = targetVqe.execAsk();
-                            if (ask) {
-                                int count = 0;
-                                if (map.containsKey(targetDataset)) {
-                                    count = map.get(targetDataset);
-                                }
-                                map.put(targetDataset, ++count);
-                            }
-                            targetGraph.close();
-                        }
-                    }
-                }
-                commonMap.put(sourceDataset, map);
-                doneList.add(sourceDataset);
-                graph.close();
-            }
-        } catch (FileNotFoundException e) {
+            client = ThriftClient.create("localhost", 15867);
+        } catch (TException e) {
             e.printStackTrace();
-        } catch (IOException e) {
+            System.exit(1);
+        }
+
+        long ns = 0;
+        try {
+            if (!client.namespace_exists("framework")) {
+                client.namespace_create("framework");
+            }
+            ns = client.namespace_open("framework");
+        } catch (TException e) {
             e.printStackTrace();
         }
+
+        String hqlQuery = "SELECT * from subgraphs where type = 'vertex'";
+
+        HqlResult hqlResult = null;
+        try {
+            hqlResult = client.hql_query(ns, hqlQuery);
+            if (hqlResult.getCells().size() > 0) {
+                for (Cell cell : hqlResult.getCells()) {
+                    ByteBuffer graphBuffer = client.get_cell(ns, "subgraphs", cell.getKey().getRow(), "graph");
+                    String graph = new String(graphBuffer.array(), graphBuffer.position(), graphBuffer.remaining());
+                    if (!datasetList.contains(graph.replace(".g", ""))) {
+                        datasetList.add(graph.replace(".g", ""));
+                    }
+                }
+            }
+        } catch (TException e) {
+            e.printStackTrace();
+        }
+
+        Set<String> doneList = new HashSet<String>();
+        Map<String, Map<String, Integer>> commonMap = new HashMap<String, Map<String, Integer>>();
+        Map<String, Set<String>> exclusionMap = new HashMap<String, Set<String>>();
+        for (String sourceDataset : datasetList) {
+            logger.info(String.format("Analyzing %s dataset...", sourceDataset));
+            Map<String, Integer> map = new HashMap<String, Integer>();
+            assert connectionURL != null;
+            VirtGraph graph = new VirtGraph("http://" + sourceDataset, connectionURL.toString(), prop.getProperty("virtuoso_user"), prop.getProperty("virtuoso_password"));
+            Query query = QueryFactory.create("SELECT DISTINCT ?p ?o WHERE { ?s ?p ?o }");
+            VirtuosoQueryExecution vqe = VirtuosoQueryExecutionFactory.create(query, graph);
+            ResultSet results = vqe.execSelect();
+            while (results.hasNext()) {
+                QuerySolution result = results.next();
+                RDFNode predicate = result.get("p");
+                RDFNode object = result.get("o");
+                //System.out.println(String.format("%s - %s", predicate, object));
+                if (!exclusionMap.containsKey(predicate.toString())) {
+                    Set<String> datasetSet = new HashSet<String>();
+                    for (String dataset : datasetList) {
+                        if (!dataset.equals(sourceDataset)) {
+                            VirtGraph targetGraph = new VirtGraph("http://" + dataset, connectionURL.toString(), prop.getProperty("virtuoso_user"), prop.getProperty("virtuoso_password"));
+                            Query predicateQuery = QueryFactory.create(String.format("ASK {?s <%s> ?o}", predicate));
+                            VirtuosoQueryExecution vqePredicate = VirtuosoQueryExecutionFactory.create(predicateQuery, targetGraph);
+                            boolean ask = vqePredicate.execAsk();
+                            if (!ask) {
+                                datasetSet.add(dataset);
+                            }
+                        }
+                    }
+
+                    exclusionMap.put(predicate.toString(), datasetSet);
+                }
+
+                for (String targetDataset : datasetList) {
+                    if ((!doneList.contains(targetDataset)) && (!targetDataset.equals(sourceDataset)) && (!exclusionMap.get(predicate.toString()).contains(targetDataset))) {
+                        VirtGraph targetGraph = new VirtGraph("http://" + targetDataset, connectionURL.toString(), prop.getProperty("virtuoso_user"), prop.getProperty("virtuoso_password"));
+                        Query targetQuery;
+                        if (object.isLiteral()) {
+                            targetQuery = QueryFactory.create(String.format("ASK { ?s <%s> \"%s\"}", predicate, object.toString().replace("\"", "").replace("\\", "")));
+                        } else {
+                            targetQuery = QueryFactory.create(String.format("ASK { ?s <%s> <%s>}", predicate, object));
+                        }
+                        VirtuosoQueryExecution targetVqe = VirtuosoQueryExecutionFactory.create(targetQuery, targetGraph);
+                        boolean ask = targetVqe.execAsk();
+                        if (ask) {
+                            int count = 0;
+                            if (map.containsKey(targetDataset)) {
+                                count = map.get(targetDataset);
+                            }
+                            map.put(targetDataset, ++count);
+                        }
+                        targetGraph.close();
+                    }
+                }
+            }
+            commonMap.put(sourceDataset, map);
+            doneList.add(sourceDataset);
+            graph.close();
+        }
+
         logger.info("End!");
     }
 }
